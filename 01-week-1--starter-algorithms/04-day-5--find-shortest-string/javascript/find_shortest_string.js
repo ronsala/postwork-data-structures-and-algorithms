498ab7e833d5b24eb8deff33ba7a2efd928bbc0e
RonSala <ron.sala@gmail.com>
2022-06-05 06:20:32 -0400

    belly auto-commit

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
@@ -1,5 +1,13 @@
 function findShortestString(arr) {
-  // type your code here
+  let shortest = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length < shortest.length) {
+      shortest = arr[i];
+    }
+  }
+
+  return shortest;
 }
 
 if (require.main === module) {
@@ -17,10 +25,59 @@ if (require.main === module) {
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
+  console.log("");
+
+  console.log("Expecting: 'Ron'");
+  console.log("=>", findShortestString(['ReBecca', 'Groot', 'Ron', 'Arya']));
+
+  console.log("");
+
+  console.log("Expecting: 'Maine'");
+  console.log("=>", findShortestString(['Mississippi', 'Maine', 'Montana', 'Maryland']));
+
   // BENCHMARK HERE
+    const startTime = Date.now();
+    let shortInput = ["JavaScript", "Ruby", "Python"];
+    let longInput = [];
+
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < 100; i++) {
+      let string = '';
+      const length = Math.floor(Math.random() * 10) +1;
+
+      for (let j = 0; j < length; j++) {
+        const letter = letters.charAt(Math.floor(Math.random() * letters.length));
+        string += letter;
+      }
+
+      longInput.push(string);
+    }
+
+    for (let i = 0; i < 1000; i++) {
+      findShortestString(shortInput);
+      findShortestString(longInput);
+    }
+
+    let endTime = Date.now();
+    let runtime = endTime - startTime;
+
+    console.log(`Runtime: ${runtime} millisecond(s)`);
+    console.log(`Average runtime: ${runtime / 2000} millisecond(s)`);
 }
 
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
+/*
+- Iterate through array
+- Set first string to variable
+- If any string is shorter than the string stored with the variable, overwrite it
+- Return the variable's string
+*/
+
+
 // And a written explanation of your solution
+/*
+Uses a for loop to check each element of the input array and keep track of the shortest string. In order to be counted as such, each checked string must be shorter than the one already saved, meaning if more than one string is equal length to the another in the array, the one earliest listed will be returned.
+*/
