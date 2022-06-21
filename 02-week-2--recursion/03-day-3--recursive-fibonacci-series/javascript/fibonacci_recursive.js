861f1c89ff7daba76bf94f2248d9c81bd35753a6
RonSala <ron.sala@gmail.com>
2022-06-21 10:01:30 -0400

    belly auto-commit

--- 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
+++ 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
@@ -1,5 +1,9 @@
 function fibonacci(n) {
-  // type your code here
+  if (n <= 1) {
+    return n
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2)
+  }
 }
 
 if (require.main === module) {
@@ -16,9 +20,27 @@ if (require.main === module) {
 
   console.log("Expecting: 55");
   console.log("=>", fibonacci(10));
+
+  console.log("");
+
+  console.log("Expecting: 2");
+  console.log("=>", fibonacci(3));
+
+  console.log("");
+
+  console.log("Expecting: 8");
+  console.log("=>", fibonacci(6));
 }
 
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+/*
+- Base case 1: Return number itself if < 2.
+- Base case 2: Otherwise, return either an expression adding the return values of recursive calls.
+*/
+
 // And a written explanation of your solution
+/*
+Function keeps calling itself to derive the sum of the return values of when the number - 1 and number -2 are passed in to it. That is it sums the previous fib numbers to get the current one.
+*/
