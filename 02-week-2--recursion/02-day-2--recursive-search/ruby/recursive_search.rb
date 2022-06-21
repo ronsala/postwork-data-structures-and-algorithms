c3c7dba38e5d7b838c1914bc1badf35cdf85e238
RonSala <ron.sala@gmail.com>
2022-06-21 06:06:18 -0400

    belly auto-commit

--- 02-week-2--recursion/02-day-2--recursive-search/ruby/recursive_search.rb
+++ 02-week-2--recursion/02-day-2--recursive-search/ruby/recursive_search.rb
@@ -1,5 +1,15 @@
 def recursive_search(arr, target)
-  # type your code in here
+  if arr.count === 0
+    return false
+  end
+
+   if arr[0] === target
+    return true
+   end
+
+  arr.slice!(0)
+
+  return recursive_search(arr, target)
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -11,8 +21,26 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: false"
   puts "=>", recursive_search([3, 2, 1], 4)
 
+  puts
+
   # Don't forget to add your own!
+  puts "Expecting: true"
+  puts "=>", recursive_search(['lion', 'tiger', 'zebra'], 'zebra')
+
+  puts
+
+  puts "Expecting: false"
+  puts "=>", recursive_search(['Harry', 'Ron'], 'Luna')
 end
 
 # Please add your pseudocode to this file
+=begin
+- Base cases: Target found or last element checked without match.
+- Each recursion, compare the first element to the target then, if they match, return true, otherwise, remove it from the array.
+- If the array is empty or reduced to one element without a match, return false.
+=end
+
 # And a written explanation of your solution
+=begin
+Function checks one element of array against target each time it's called. If, during an execution, a conditional is not satisfied to return true or false, the first element is removed from the array and the function called again.
+=end
