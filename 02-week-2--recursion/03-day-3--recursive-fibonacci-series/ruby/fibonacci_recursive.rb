bb140762c538ec06bd6a1c7c2c8ffcfadffa78be
RonSala <ron.sala@gmail.com>
2022-06-21 10:14:37 -0400

    belly auto-commit

--- 02-week-2--recursion/03-day-3--recursive-fibonacci-series/ruby/fibonacci_recursive.rb
+++ 02-week-2--recursion/03-day-3--recursive-fibonacci-series/ruby/fibonacci_recursive.rb
@@ -1,5 +1,5 @@
 def fibonacci(n)
-  # type your code in here
+  n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2)
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -17,7 +17,25 @@ if __FILE__ == $PROGRAM_NAME
   puts "=>", fibonacci(10)
 
   # Don't forget to add your own!
+  puts
+
+  puts "Expecting: 2"
+  puts "=>", fibonacci(3)
+
+  puts
+
+  puts "Expecting: 8"
+  puts "=>", fibonacci(6)
+
 end
 
 # Please add your pseudocode to this file
+=begin
+- Base case 1: Return number itself if < 2.
+- Base case 2: Otherwise, return an expression adding the return values of recursive calls.
+=end
+
 # And a written explanation of your solution
+=begin
+Function keeps calling itself to derive the sum of the return values of when the number - 1 and number -2 are passed in to it. That is it sums the previous fib numbers to get the current one.
+=end
