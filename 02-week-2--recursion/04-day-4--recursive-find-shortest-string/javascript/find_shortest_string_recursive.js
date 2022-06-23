a803ffdd273b9391823c2c639a22a29fdd953552
RonSala <ron.sala@gmail.com>
2022-06-23 05:59:28 -0400

    belly auto-commit

--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
@@ -1,9 +1,14 @@
 function findShortestStringRecursive(arr) {
-  // type your code here
+  if (arr.length === 1) {
+    return arr[0];
+  } else if (arr[0].length > arr[1].length) {
+    return findShortestStringRecursive(arr.slice(1));
+  } else {
+    return findShortestStringRecursive([...[arr[0]], ...arr.slice(2)]);
+  }
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: 'a'");
   console.log("=>", findShortestStringRecursive(['aaa', 'a', 'bb', 'ccc']));
 
@@ -16,9 +21,30 @@ if (require.main === module) {
 
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestStringRecursive(['flower', 'juniper', 'lily', 'dandelion']));
+
+  // add your own tests in here
+  console.log("");
+
+  console.log("Expecting: 'Ron'");
+  console.log("=>", findShortestStringRecursive(['ReBecca', 'Groot', 'Ron', 'Arya']));
+
+  console.log("");
+
+  console.log("Expecting: 'Maine'");
+  console.log("=>", findShortestStringRecursive(['Mississippi', 'Maine', 'Montana', 'Maryland']));
 }
 
 module.exports = findShortestStringRecursive;
 
 // Please add your pseudocode to this file
+/*
+- Base case: 
+    Return the lone element when arr is length 1 or has been reduced to length 1 by recursive calls.
+- Remove first element of array if longer than the second element.
+- Remove the second element of array if not shorter than the first element.
+*/
+
 // And a written explanation of your solution
+/*
+If the array has one element, return that element. Otherwise, compare the lengths of the first and second element and remove the one that's longer with a preference for the first occurring until the array is reduced to one element.
+*/
