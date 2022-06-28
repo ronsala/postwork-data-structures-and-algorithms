4eb2350d5e94d8f8eb192d3f0f94044488307000
RonSala <ron.sala@gmail.com>
2022-06-28 06:23:36 -0400

    belly auto-commit

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
@@ -1,5 +1,14 @@
 function selectionSortRecursive(arr) {
-  // type your code here
+  if (arr.length === 0) {
+    return arr;
+  }
+
+  const min = Math.min(...arr);
+  arr.splice(arr.indexOf(min), 1);
+
+  let result = selectionSortRecursive(arr);
+  result.unshift(min);
+  return result;
 }
 
 if (require.main === module) {
@@ -8,9 +17,32 @@ if (require.main === module) {
   console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
 
   console.log("");
+
+  console.log("Expecting: [-1, 3, 5, 5, 6]");
+  console.log("=>", selectionSortRecursive([3, -1, 5, 6, 5]));
+
+  console.log("");
+
+  console.log("Expecting: [-6, -5, -5, -3, -1]");
+  console.log("=>", selectionSortRecursive([-3, -1, -5, -6, -5]));
+
+  console.log("");
 }
 
 module.exports = selectionSortRecursive;
 
 // Please add your pseudocode to this file
+/*
+Base case: Return arr if arr.length === 0.
+= find the min of arr
+= find the index of min
+- delete min from arr
+- call the function
+- add min to beginning of array returned by function call
+- return the modified array
+*/
+
 // And a written explanation of your solution
+/*
+Each step finds and removes the smallest element element from the array and returns an array returned from a recursive cal with that element at the beginning. When all elements have been removed from the original array, execution ends.
+*/
