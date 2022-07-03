3a64bf7e58e6f4faed1a8965ddd5801ad8a96409
RonSala <ron.sala@gmail.com>
2022-07-03 05:39:59 -0400

    belly auto-commit

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/benchmark/benchmark.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/benchmark/benchmark.js
@@ -1,17 +1,45 @@
 // Paste your iterative and recursive solutions in this file
 // And then calculate their average run times to compare them.
 
+const arr = [3, -1, 5, 2];
 
+function selectionSort() {
+  let output = [];
+  let length = arr.length;
 
+  for (let i=0; i < length; i++) {
+    let lowest = Math.min(...arr);
+    let index = arr.indexOf(lowest);
+    arr.splice(index, 1);
+    output.push(lowest);
+  }
 
+  return output;
+}
 
+function selectionSortRecursive() {
+  if (arr.length === 0) {
+    return arr;
+  }
+
+  const min = Math.min(...arr);
+  arr.splice(arr.indexOf(min), 1);
+
+  let result = selectionSortRecursive(arr);
+  result.unshift(min);
+  return result;
+}
 
 function benchmark(callback) {
   const startTime = Date.now();
 
-  for (let i = 0; i < 1000; ++i) {
-    callback();
+  for (let i = 0; i < 100000000; ++i) {
+    callback(arr);
   }
 
-  return (Date.now() - startTime) / 1000;
+  return (Date.now() - startTime) / 100000000;
 }
+
+console.log(benchmark(selectionSort))
+
+console.log(benchmark(selectionSortRecursive))
